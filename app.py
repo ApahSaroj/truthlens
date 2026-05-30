d092359814d4a2799a7109f257f3bedb77fe0c81
ApahSaroj <84530564+ApahSaroj@users.noreply.github.com>
2026-05-30 14:26:53 +0530

    Initialize FastAPI app with router and home endpoint

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI
+from api.routes import router
 
+app = FastAPI(
+    title="TruthLens",
+    version="0.1"
+)
+
+app.include_router(router)
+
+@app.get("/")
+def home():
+    return {
+        "project": "TruthLens",
+        "status": "running"
+    }
